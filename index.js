ca48815e20527d861099477d775bf49dd7329968
Cameron Chandler <webstormcam@gmail.com>
2021-06-15 08:47:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ document.getElementById('usernameInput').addEventListener('keyup',function(event
     console.log(username);
     
 })
+
+let allNamesDOMCollection=[]
+let ul=document.getElementById('results')
+let items=ul.getElementsByTagName('li')
+for(var i=0; i<items.length;i++){
+     allNamesDOMCollection.push(items[i])
+}
+
